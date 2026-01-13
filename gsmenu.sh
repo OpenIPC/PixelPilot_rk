4b535d02f27234b31a2a601de5c1608bd99b259f
julian-boetzel <216579141+julian-boetzel@users.noreply.github.com>
2026-01-13 13:59:47 +0100

    Add modified gsmenu.sh (#3)
    * Add OSD functionality
    
    adds AALINK OSD Level slider and OSD Signal Bars toggle
    
    * Update air_aalink.c
    
    change osd level to dropdown to be more similar to WFB-NG
    
    * Update air_aalink.c
    
    yeah no comment
    
    * Update air_aalink.c
    
    Its a dropdown, not a slider
    
    * Update gsmenu.sh
    
    for OSD_LEVEL and SHOW_SIGNAL_BARS functionality

--- gsmenu.sh
+++ gsmenu.sh
@@ -177,6 +177,12 @@ case "$@" in
     "values air aalink OSD_SCALE")
         echo -n 0.2 2
         ;;
+    "values air aalink OSD_LEVEL")
+        echo -n -e "0\n1\n2\n3"
+        ;;
+    "values air aalink SHOW_SIGNAL_BARS")
+        echo -n -e "true\nfalse"
+        ;;
     "values air aalink THROUGHPUT_PCT")
         echo -n 0 100
         ;;
