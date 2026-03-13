c4c477d2f1d8712319c2c8507e76bf53af56366d
Sebastiaan Rutgers <sebastiaan_rutgers@hotmail.com>
2026-03-13 10:21:22 +0100

    Place valve before queue in restream branch, add silent=true

--- src/gstrtpreceiver.cpp
+++ src/gstrtpreceiver.cpp
@@ -230,8 +230,8 @@ namespace {
 
     static std::string create_restream_branch() {
         std::stringstream ss;
-        ss << " rtp_tee. ! queue leaky=downstream max-size-buffers=10 max-size-bytes=0 max-size-time=0"
-              " ! valve name=restream_valve drop=true"
+        ss << " rtp_tee. ! valve name=restream_valve drop=true"
+              " ! queue leaky=downstream max-size-buffers=10 max-size-bytes=0 max-size-time=0 silent=true"
               " ! udpsink name=restream_sink host=0.0.0.0 port=5600 sync=false async=false qos=false";
         return ss.str();
     }
